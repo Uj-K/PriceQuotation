9e0f874d915f48cc94f9042faf8a735f9deab7fe
Uj-K <umkrall@gmail.com>
2025-02-11 00:23:14 -0800

    Edit Total and DiscountAmount

--- PriceQuotation/Models/PriceQuotation.cs
+++ PriceQuotation/Models/PriceQuotation.cs
@@ -5,18 +5,24 @@ namespace PriceQuotation.Models
     
     public class PriceQuotation
     {
+        /// <summary>
+        /// The sale price  
+        /// </summary>
         [Required(ErrorMessage = "Subtotal price is required.")]
         [Range(0.00, double.MaxValue, ErrorMessage = "Subtotal price must to be grater than 0")]
         public decimal Subtotal { get; set; }
 
+        /// <summary>
+        /// The present of discount rate
+        /// </summary>
         [Required]
         [ Range(0, 100, ErrorMessage = "Discount Percent must to be a valid number from 0 to 100.")]
         public int DiscountPercent { get; set; }
 
 
-        public decimal DiscountAmount { get; set; }
+        public decimal DiscountAmount => Subtotal * (DiscountAmount / 100);
 
-        public decimal Total { get; set; }
+        public decimal Total => Subtotal - DiscountAmount;
 
     }
 }
