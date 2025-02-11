ce300a199a88c884e243523f216277341511098a
Uj-K <umkrall@gmail.com>
2025-02-10 23:59:15 -0800

    Add project files.

---
+++ PriceQuotation/Models/PriceQuotation.cs
@@ -0,0 +1,22 @@
+﻿namespace PriceQuotation.Models
+{
+    
+    public class PriceQuotation
+    {
+        public int Subtotal { get; set; }
+
+        public decimal DiscountPercent { get; set; }
+
+        public decimal DiscountAmount { get; set; }
+
+        public decimal Total { get; set; }
+
+    }
+}
+
+/*• Use the MVC pattern. 
+ * To do that, create a model class that stores 
+ * the subtotal and discount percent 
+ * and calculates the discount amount and total. 
+ * Make sure to bind that model to the Razor view 
+ * that displays the Price Quotation page shown above.*/
